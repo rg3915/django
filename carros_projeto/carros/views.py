5c6321c45f801a6a4e1175201b3c444798970ef9
rg3915 <rg3915@yahoo.com.br>
2013-11-22 01:35:41 -0200

    funcao adiciona

--- carros_projeto/carros/views.py
+++ carros_projeto/carros/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render_to_response
-from carros.models import Carro
+# -*- encoding: utf-8 -*-
 
-def index(request):
-	return render_to_response('carros/index.html',{
-		'carros': Carro.objects.all().order_by('id')
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponse
+from carros.models import Carro
+from carros.forms import FormCarro
+
+#def index(request):
+#	return render_to_response('index.html',{
+#		'carros': Carro.objects.all().order_by('id')
+#		})
+
+def lista(request):
+	lista_itens = Carro.objects.all()
+	return render_to_response('lista.html',{
+		'carros': lista_itens
 		})
+
+def adiciona(request):
+	if request.method == 'POST':
+		form = FormCarro(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return render_to_response("salvo.html", {})
+	else:
+		form = FormCarro()
+	return render_to_response("adiciona.html", {'form': form},
+		context_instance=RequestContext(request))
+
+def item(request, nr_item):
+	item = get_object_or_404(Carro, pk=nr_item)
+	return render_to_response("item.html", {'item': item})
+
