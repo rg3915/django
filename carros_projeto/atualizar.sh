a116f2d4a71af2f249a78638c514d4e66969b274
rg3915 <rg3915@yahoo.com.br>
2013-11-26 02:37:38 -0200

    bootstrap

---
+++ carros_projeto/atualizar.sh
@@ -0,0 +1,27 @@
+git add carros/forms.py
+git commit -m "forms"
+git add carros/views.py
+git commit -m "funcao adiciona"
+git add carros_projeto/settings.py
+git commit -m "novo path em TEMPLATE_DIRS"
+git add carros_projeto/urls.py
+git commit -m "urls para forms"
+
+git add static/estilo.css
+git commit -m "css"
+
+git add templates/carros/adiciona.html
+git commit -m "adiciona"
+git add templates/carros/base.html
+git commit -m "base"
+git add templates/carros/index.html
+git commit -m "index"
+git add templates/carros/item.html
+git commit -m "item"
+git add templates/carros/lista.html
+git commit -m "lista"
+git add templates/carros/salvo.html
+git commit -m "salvo"
+
+git add all
+git commit -m "mais alguns ajustes"
