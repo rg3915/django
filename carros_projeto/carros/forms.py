157c5eeee22cee1863b6eed19e697aeb03d0d427
rg3915 <rg3915@yahoo.com.br>
2013-12-08 00:03:25 -0200

    novos forms marca e modelo

--- carros_projeto/carros/forms.py
+++ carros_projeto/carros/forms.py
@@ -1,8 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 from django import forms
-from models import Carros
+from models import *
 
 class FormCarro(forms.ModelForm):
 	class Meta:
 		model = Carros
+
+class FormMarca(forms.ModelForm):
+	class Meta:
+		model = marcas
+
+class FormModelo(forms.ModelForm):
+	class Meta:
+		model = modelos
