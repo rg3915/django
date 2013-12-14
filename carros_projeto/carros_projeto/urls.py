52b61408c8bb931094773d8db3165b92be188100
rg3915 <rg3915@yahoo.com.br>
2013-12-14 01:08:41 -0200

    home

--- carros_projeto/carros_projeto/urls.py
+++ carros_projeto/carros_projeto/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'carros.views.lista', name='lista_carro'),
+    url(r'^$', 'carros.views.index_view', name='home'),
     url(r'adiciona/$', 'carros.views.adiciona', name='adiciona_carro'),
     url(r'item/(?P<nr_item>\d+)/$', 'carros.views.item', name='item_carro'),
     url(r'listamarca/$', 'carros.views.listaMarca', name='lista_marca'),
