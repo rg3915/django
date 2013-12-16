a50ebbd8c46d4d538d3797728eace02d41a1b9cc
rg3915 <rg3915@yahoo.com.br>
2013-12-15 23:56:57 -0200

    moeda

--- carros_projeto/carros/models.py
+++ carros_projeto/carros/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-   
 from django.db import models
 
-class marcas(models.Model):
+class Marca(models.Model):
 	marca = models.CharField('Marca', max_length=50)
 
 	class Meta:
@@ -11,9 +11,9 @@ class marcas(models.Model):
 	def __unicode__(self):
 		return self.marca
 
-class modelos(models.Model):
+class Modelo(models.Model):
 	modelo = models.CharField('Modelo', max_length=50)
-	marca = models.ForeignKey(marcas)
+	marca = models.ForeignKey(Marca)
 
 	class Meta:
 		verbose_name=u'modelo'
@@ -22,14 +22,22 @@ class modelos(models.Model):
 	def __unicode__(self):
 		return self.modelo
 
-class Carros(models.Model):
+class Veiculo(models.Model):
 	veiculo = models.CharField('Veículo',max_length=50)
-	modelo = models.ForeignKey(modelos)
+	modelo = models.ForeignKey(Modelo)
 	preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
 
 	class Meta:
-		verbose_name=u'carro' 
-		verbose_name_plural=u'carros'   
+		verbose_name=u'veículo' 
+		verbose_name_plural=u'veículos'   
 
 	def __unicode__(self):
 		return self.veiculo
+
+	def moeda(self):
+		p = str(self.preco)
+		if p != None:
+			import locale
+			locale.setlocale( locale.LC_ALL, '')
+			return locale.currency( p, grouping=True )
+		return ''
