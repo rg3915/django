d1efebc5bfe8f7c738b6fef1e9a4f502bda4a4f4
rg3915 <rg3915@yahoo.com.br>
2013-12-15 22:56:24 -0200

    novos urls

--- carros_projeto/carros_projeto/urls.py
+++ carros_projeto/carros_projeto/urls.py
@@ -5,19 +5,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'carros_projeto.views.home', name='home'),
-    # url(r'^carros_projeto/', include('carros_projeto.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'carros.views.index_view', name='home'),
-    url(r'adiciona/$', 'carros.views.adiciona', name='adiciona_carro'),
-    url(r'item/(?P<nr_item>\d+)/$', 'carros.views.item', name='item_carro'),
-    url(r'listamarca/$', 'carros.views.listaMarca', name='lista_marca'),
-    url(r'adicionamarca/$', 'carros.views.adicionaMarca', name='adiciona_marca'),
-    url(r'adicionamodelo/$', 'carros.views.adicionaModelo', name='adiciona_modelo'),
+    url(r'^$', 'carros.views.indexView', name='home'),
+    url(r'back_office/$', 'carros.views.backOffice', name='back_office'),
+    url(r'lista_veiculo/$', 'carros.views.listaVeiculo', name='lista_veiculo'),
+    url(r'item_veiculo/(?P<nr_item>\d+)/$', 'carros.views.itemVeiculo', name='item_veiculo'),
+    url(r'adiciona_veiculo/$', 'carros.views.adicionaVeiculo', name='adiciona_veiculo'),
+    url(r'lista_marca/$', 'carros.views.listaMarca', name='lista_marca'),
+    url(r'item_marca/(?P<nr_item>\d+)/$', 'carros.views.itemMarca', name='item_marca'),
+    url(r'adiciona_marca/$', 'carros.views.adicionaMarca', name='adiciona_marca'),
+    url(r'lista_modelo/$', 'carros.views.listaModelo', name='lista_modelo'),
+    url(r'item_modelo/(?P<nr_item>\d+)/$', 'carros.views.itemModelo', name='item_modelo'),
+    url(r'adiciona_modelo/$', 'carros.views.adicionaModelo', name='adiciona_modelo'),
 )
