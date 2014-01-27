f6ce3fde82e4b5987041654824d57fabe7eb2442
rg3915 <rg3915@yahoo.com.br>
2014-01-27 02:16:19 -0200

    TemplateView e ListView

--- pedido/pedido_project/pedido/views.py
+++ pedido/pedido_project/pedido/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# encoding: utf-8
+from django.views.generic import TemplateView, ListView
+from pedido.models import *
+
+class index(TemplateView):
+	template_name = 'pedido/index.html'
+
+class listaProduto(ListView):
+	template_name = 'pedido/lista_produto.html'
+	model = Produto
+	context_object_name = 'produtos'
--- pedido/pedido_project/pedido_project/urls.py
+++ pedido/pedido_project/pedido_project/urls.py
@@ -1,9 +1,14 @@
+# encoding: utf-8
 from django.conf.urls import patterns, include, url
+from pedido.views import *
+from pedido.models import *
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'pedido_project.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^$', 'pedido_project.views.home', name='home'),
+    url(r'^$', index.as_view(), name='home'),
+    url(r'^lista_produto/$', listaProduto.as_view(), name='lista_produto'),
 )
