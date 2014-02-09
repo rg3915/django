becfd86726553aefd6744fd504f31ca5bf3adf82
rg3915 <rg3915@yahoo.com.br>
2014-02-09 21:26:41 -0200

    demo ja era

--- demo/home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from home.models import *
-#from home.models import userProfile
-
-#admin.site.register(userProfile)
-admin.site.register((Produto, CategoriaProduto))
--- demo/home/views.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-from django.views.generic import TemplateView
-from home.models import *
-
-class index(TemplateView):
-	template_name = 'home/index.html'
